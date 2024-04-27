ff9615678db9f8eeced70127577be51232a57daa
elsayedebiad <153118753+elsayedebiad@users.noreply.github.com>
2024-04-27 22:59:41 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const port = 3002;
+const mongoose = require("mongoose");
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+var methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+const allroutes = require("./routes/allroutes");
+
+
+// Auto refresh
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "public"));
+
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://engelsayedebed:Mk$HYH7mDtwh_hT@cluster0.wmmzqss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(allroutes);
